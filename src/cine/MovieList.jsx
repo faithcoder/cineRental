2f822652a3f4fad7170d254996af6287a9187132
Abdullah Al Arif <48954386+faithcoder@users.noreply.github.com>
2024-01-23 00:00:17 +0600

    error

--- src/cine/MovieList.jsx
+++ src/cine/MovieList.jsx
@@ -11,11 +11,7 @@ export default function MovieList() {
             id={movie.id}
             className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl"
           >
-            <img
-              className="w-full object-cover"
-              src={getImgUrl(movie.cover)}
-              alt=""
-            />
+            <img className="w-full object-cover" src={getImgUrl()} alt="" />
             <figcaption className="pt-4">
               <h3 className="text-xl mb-1">{movie.title}</h3>
               <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
